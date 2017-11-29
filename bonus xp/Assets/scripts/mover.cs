0b530c38f8edc76b84c756e3755040eb3d83bcbd
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-11-29 09:09:16 -0500

    temps

---
+++ bonus xp/Assets/scripts/mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+	// Public variables
+	public float speed;
+	public GameObject crossBow;
+
+	// Private variables
+	private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start()
+	{
+		rBody = this.GetComponent<Rigidbody2D>();
+		rBody.velocity = this.transform.right * speed;
+	}
+}
---
+++ bonus xp/Assets/scripts/playerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+	//public varriables
+	public float speed = 5.0f;
+	public float groundRadius; //radius of the groundcheck
+	public float jumpForce = 700f;
+
+	public Transform groundCheck;
+	public LayerMask whatIsGround; //layers of that player can jump on (this case everything)
+
+
+	//ground check
+	bool grounded = false;
+	float timer;
+
+	Rigidbody2D rBody;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		rBody = this.GetComponent<Rigidbody2D>();
+
+	}
+
+	private void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround); //check if groundcheck overlap with the ground
+		float horizMove = Input.GetAxis("Horizontal");
+
+		rBody.velocity = new Vector2(horizMove * speed, rBody.velocity.y); // basic movement
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		if ((grounded) && Input.GetButtonDown("Jump")) //checking for the input and if groundcheck is overlapping with the ground
+		{
+
+			rBody.AddForce(new Vector2(0, jumpForce));
+
+		}
+	}
+}
